e37738b1d52952175b147008c072638984454421
Eric Bastos Barbosa <163416463+Eric1557@users.noreply.github.com>
2025-10-28 12:05:20 -0300

    Gerenciador de Tarefas
    Código comentado.

--- src/GerenciadorTarefas.java
+++ src/GerenciadorTarefas.java
@@ -1,21 +1,23 @@
 import java.util.List;
 import java.util.ArrayList;
 
+//Classe principal que gerencia uma lista de tarefas
 public class GerenciadorTarefas {
-
+   //Lista principal que gerencia uma lista de tarefas
   List<Tarefa> tarefas = new ArrayList<>();
+  //Variável para controlar o próximo ID disponível
   private int proxId = 1;
-
+  //Construtor que inicializa o gerenciador com uma lista de tarefas
   public GerenciadorTarefas(List<Tarefa> tarefas) {
     this.tarefas = tarefas;
   }
-
+  // Método para adicionar uma nova tarefa á lista
   public void adicionarTarefas(String nome, String descricao) {
      tarefas.add(new Tarefa(proxId++,nome,descricao));
      System.out.println("Tarefa "  + nome + " adicionada com sucesso!\n ");
   
   }
-
+  // Método para listar todas as tarefas á lista
   public void ListarTarefas() {
     if (tarefas.isEmpty()) {
       System.out.println("Lista de tarefas vazia !");
@@ -26,7 +28,7 @@ public class GerenciadorTarefas {
       }
     }
   }
-
+  // Método para remover uma tarefa com base no ID
   public void RemoverTarefas(int id) {
     for (Tarefa t : tarefas) {
       if (id == t.getId()) {
@@ -37,7 +39,7 @@ public class GerenciadorTarefas {
     }
     System.out.println("Erro : As Tarefas não foram removidas !");
   }
-
+  // Método para atualizar os dados de uma tarefa existente
   public void AtualizarTarefas(int id, String nome, String descricao, boolean concluida) {
     for (Tarefa t : tarefas) {
       t.setNome(nome);
@@ -48,7 +50,7 @@ public class GerenciadorTarefas {
     }
     System.out.println("Erro : As Tarefas não foram atualizadas !");
   }
-
+  // Método para marcar uma tarefa como concluída
   public void MarcarConcluida(int id) {
     for (Tarefa t : tarefas) {
       if (id == t.getId()) {
